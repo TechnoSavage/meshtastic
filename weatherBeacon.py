03de98e7b419833c1a85c9c36e26b5d17db738b8
TechnoSavage <technosavage@gmail.com>
2024-01-07 10:12:11 -0500

    .

--- weatherBeacon.py
+++ weatherBeacon.py
@@ -6,12 +6,13 @@ import time
 from meshClass import meshtasticConn
 
 def getForecast(token, lat, lon): 
-    """ Retrieve Tasks from Organization corresponding to supplied token.
+    """ Get forecast information for supplied lat and lon.
 
-           :param url: A string, URL of the runZero console.
-           :param token: A string, Account API Key.
-           :returns: A JSON object, runZero task data.
-           :raises: ConnectionError: if unable to successfully make GET request to console."""
+           :param token: A string, openweathermap.org API Key.
+           :param lat: A string, latitude of target location.
+           :param lon: A string, longitude of target location.
+           :returns: A JSON object, forecast data.
+           :raises: ConnectionError: if unable to successfully make GET request."""
     
     url = "http://api.openweathermap.org/data/2.5/forecast?"
     payload = {'appid': token,
@@ -26,20 +27,22 @@ def getForecast(token, lat, lon):
         raise error
     
 def parseForecast(forecast):
+    """ """
     weather = forecast.decode("utf-8")
     loaded = json.loads(weather)
     print(json.dumps(loaded, indent=4))
+    return "testing"
     
 def main():
     port = os.environ["SERIAL_PORT"]
-    #conn = meshtasticConn(port)
+    conn = meshtasticConn(port)
     token = os.environ["OWM_KEY"]
     lat = os.environ["LATITUDE"]
     lon = os.environ["LONGITUDE"]
     while True:
         forecast = getForecast(token, lat, lon)
         conditions = parseForecast(forecast)
-        #conn.sendMessage(weather)
+        conn.sendMessage(conditions)
         time.sleep(3600)
 
 if __name__ == "__main__":
